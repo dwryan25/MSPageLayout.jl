bc06461040632028059086c9e18ddc59fd82a5a5
Neel Smith <dnsmith.neel@gmail.com>
2023-07-09 06:52:01 -0400

    Add basic numeric utils on boxed pair

--- src/boxedtext.jl
+++ src/boxedtext.jl
@@ -9,10 +9,52 @@ struct BoxedTextPair
 end
 
 
-"""Compute left edge of `scholionbox` within page zone.
+"""Compute left edge of `scholionbox`.
 $(SIGNATURES)
 """
-function scholion_x_left(textpair::BoxedTextPair)
-    CitableImage.roiFloats(textpair.scholionbox)[1]
-    
+function scholion_x_left(textpair::BoxedTextPair; digits = 3)
+    imagefloats(textpair.scholionbox, digits = digits)[1]
+end
+
+"""Compute right edge of `scholionbox` within page zone.
+$(SIGNATURES)
+"""
+function scholion_x_right(textpair::BoxedTextPair; digits = 3)
+    floats = imagefloats(textpair.scholionbox, digits = digits)
+    floats[1] + floats[3]
+end
+
+
+"""Compute center in x  of `scholionbox` within page zone.
+$(SIGNATURES)
+"""
+function scholion_x_center(textpair::BoxedTextPair; digits = 3)
+    r = scholion_x_right(textpair, digits = digits)
+    l = scholion_x_left(textpair, digits = digits)
+    (r - l) / 2
+end
+
+"""Compute top edge of `scholionbox`.
+$(SIGNATURES)
+"""
+function scholion_y_top(textpair::BoxedTextPair; digits = 3)
+    imagefloats(textpair.scholionbox, digits = digits)[2]
+end
+
+"""Compute bottom edge of `scholionbox` within page zone.
+$(SIGNATURES)
+"""
+function scholion_y_bottom(textpair::BoxedTextPair; digits = 3)
+    floats = imagefloats(textpair.scholionbox, digits = digits)
+    floats[2] + floats[4]
+end
+
+
+"""Compute center in y  of `scholionbox` within page zone.
+$(SIGNATURES)
+"""
+function scholion_y_center(textpair::BoxedTextPair; digits = 3)
+    b = scholion_x_bottom(textpair, digits = digits)
+    t = scholion_x_top(textpair, digits = digits)
+    (t - b) / 2
 end
